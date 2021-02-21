ee082e2aca18dd487d976740721cf26b8928f205
Ryota Saito <sa110.instagram@gmail.com>
2021-02-21 20:42:20 +0900

    サンプルコードの追加

--- IoT-Temperature/hello_world/app.py
+++ IoT-Temperature/hello_world/app.py
@@ -1,27 +1,23 @@
 import json
-
+import boto3
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    # 参考にしたコードのURL
+    # https://qiita.com/is_ryo/items/74f3fc70b7602888a2ac
+    try:
+        dynamoDB = boto3.resource("dynamodb")
+        table = dynamoDB.Table("table-name") # DynamoDBのテーブル名
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
+        # DynamoDBへのPut処理実行
+        table.put_item(
+            Item = {
+                "PartitionKey": "your-partition-key-data", # Partition Keyのデータ
+                "SortKey": "your-sort-key-data", # Sort Keyのデータ
+                "OtherKey": "your-other-data"  # その他のデータ
+            }
+        )
+    except Exception as e:
+        print e
 
     return {
         "statusCode": 200,
